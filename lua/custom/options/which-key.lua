f5334c4213c0857d0b2b07b8d22d8e6d4b53f99d
Kevin8031 <concilio.kevin@gmail.com>
2024-10-06 21:38:17 +0200

    fix: which-key

--- lua/custom/options/which-key.lua
+++ lua/custom/options/which-key.lua
@@ -1,3 +1,7 @@
-require('which-key').register {
-  ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
+local which_key = require 'which-key'
+
+which_key.add {
+  { '<leader>p', group = '[P]roject' },
+  { '<leader>a', group = '[A]dd buffer to harpoon' },
+  { '<leader>p_', hidden = true },
 }
