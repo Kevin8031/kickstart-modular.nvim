e07f621fa241dd02b4a872b61a4841af03014dc5
Kevin8031 <concilio.kevin@proton.me>
2026-09-20 12:53:31 +0200

    enable markdown formatter

--- lua/custom/options/conform.lua
+++ lua/custom/options/conform.lua
@@ -1,11 +1,13 @@
 require('conform').setup {
   format_on_save = function(bufnr)
     -- Disable with a global or buffer-local variable
-    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      return
-    end
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
     return { timeout_ms = 500, lsp_format = 'fallback' }
   end,
+
+  formatters_by_ft = {
+    markdown = { 'prettier' },
+  },
 }
 
 vim.api.nvim_create_user_command('ConformDisable', function(args)
