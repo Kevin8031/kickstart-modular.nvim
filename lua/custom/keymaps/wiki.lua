d55804f742b54cd8f3e5d1385c0e6e1f0f5828a9
Kevin8031 <concilio.kevin@proton.me>
2026-09-25 09:26:12 +0200

    wiki toc bindings

--- lua/custom/keymaps/wiki.lua
+++ lua/custom/keymaps/wiki.lua
@@ -1,1 +1,3 @@
 vim.keymap.set('n', '<leader>wst', ':WikiTags<CR>', { desc = 'wiki-tags' })
+vim.keymap.set('n', '<leader>wt', ':WikiToc<CR>', { desc = 'wiki-toc' })
+vim.keymap.set('n', '<leader>wT', ':WikiTocGenerate<CR>', { desc = 'wiki-toc-generate' })
