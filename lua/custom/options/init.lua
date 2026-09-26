0f992936dd96c20fc08d9a2c90582413b9f0d677
Kevin8031 <concilio.kevin@proton.me>
2026-09-26 12:36:15 +0200

    save folds on exit

--- lua/custom/options/init.lua
+++ lua/custom/options/init.lua
@@ -5,3 +5,17 @@ require 'custom.options.markdown'
 require 'custom.options.spell'
 require 'custom.options.lspconfig'
 require 'custom.options.undo'
+
+-- Save folds when closing buffer
+vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
+  pattern = { '*.*' },
+  desc = 'save view (folds), when closing file',
+  command = 'mkview',
+})
+
+-- Restore folds when closing buffer
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  pattern = { '*.*' },
+  desc = 'load view (folds), when opening file',
+  command = 'silent! loadview',
+})
